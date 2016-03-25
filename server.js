3990b2da59c563979d0912414f7ccac4470c87e9
Vale <valentina.monetta@gmail.com>
2016-03-25 14:21:18 +0000

    Added mongodb

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job')
 
 var app = express();
 
@@ -7,9 +9,26 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/jobs', function(req, res) {
+    mongoose.model('Job').find({}).exec(function(error, collection) {
+         res.send(collection);
+    })
+});
+
 app.get('*', function(req, res) {
    res.render('index');
 });
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+mongoose.connect('mongodb://vale:babyseed14@ds021989.mlab.com:21989/jobfinder');
+                  
+var con = mongoose.connection;
+
+con.once('open', function() {
+    console.log('connected to mongodb successfully!')
+    jobModel.seedJobs();
+})
+
 
 app.listen(process.env.PORT, process.env.IP);
 
