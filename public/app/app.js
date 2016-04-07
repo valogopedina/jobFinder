72ad28e9097d60bd6a001471ffb63410f6e7a542
Vale <valentina.monetta@gmail.com>
2016-04-07 21:08:00 +0000

    WIP project

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,6 @@
-angular.module('app', ['ngResource']);
+app = angular.module('app', ['ngResource']);
 
-angular.module('app').controller('testCtrl', function($scope, $resource) {
+angular.module('app').controller('testCtrl', function($scope, $resource, jobs) {
     $scope.jobs = $resource('/api/jobs').query();
+    jobs.save({title: 'test title', description: 'test description'});
 });
--- test/server/Jobs-data-spec.js
+++ test/server/Jobs-data-spec.js
@@ -2,7 +2,7 @@ var expect = require("chai").expect;
 var mongoose = require("mongoose");
 var jobModel = require('../models/Job');
 var Promise = require('bluebird');
-var jobsData = require('../jobs-data.js');
+var jobsData = require('../../jobs-data.js');
 
 function resetJobs() {
    return new Promise(function(resolve, reject) {
@@ -27,6 +27,7 @@ describe('get jobs', function(){
       });
    })
    
+
    it("should never be empty since jobs are seeded", function(){
       expect(jobs.length).to.be.at.least(1);
    });
@@ -39,3 +40,34 @@ describe('get jobs', function(){
       expect(jobs[0].description).to.not.be.empty;
    });
 });
+
+describe("db save jobs", function() {
+   
+   var job = {title: 'Cook', description: 'You will be making bagels'};
+   var jobs;
+   
+   function saveTestJob(){
+      return jobsData.saveJob(job);
+   }
+   
+    before(function(done) {
+      connectDB('mongodb://localhost/jobfinder')
+      .then(resetJobs)
+      .then(function() { return jobsData.saveJob(job) })
+      .then(jobsData.findJobs)
+      .then(function setJobs(collection) {
+         jobs = collection;
+         done();
+      });
+   })
+   
+   after(function(){
+      mongoose.connection.close();
+   })
+   
+   
+   it("should have one job after saving one job", function(){
+      expect(jobs).to.have.length(1);
+   })
+});
+ 
